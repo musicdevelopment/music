ec1717e7570199c0f59a8cf113955caf02d1b372
AleksanderEvensen-2 <ahse032@gmail.com>
2016-11-04 21:41:59 +0100

    Made Play/pause
    finaly done

--- js/mPlayer.js
+++ js/mPlayer.js
@@ -1,2 +1,22 @@
+var playB = document.getElementById('playB');
+
+
+var isPlaying = false;
 var audio = new Audio("audio/bestmusic.mp3");
-audio.play();
+
+
+playB.onclick = function(){
+  if(isPlaying === false){
+    playB.src = "images/pauseBu.png";
+    audio.play();
+    isPlaying = true;
+    return;
+  }
+
+  if(isPlaying === true){
+    playB.src = "images/playBu.png";
+    audio.pause();
+    isPlaying = false;
+    return;
+  }
+}
