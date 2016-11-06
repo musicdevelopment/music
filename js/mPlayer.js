70b8bb96af58402efdbe5dd311cb9883af8405d5
AleksanderEvensen-2 <ahse032@gmail.com>
2016-11-06 20:07:17 +0100

    adding songs is posible now not all fixing soon
    Done long time coding

--- js/mPlayer.js
+++ js/mPlayer.js
@@ -1,25 +1,104 @@
+
+
+
+// Grab all player buttons
+
 var playB = document.getElementById('playB');
+
 var skipCu = document.getElementById('skipCu');
+
 var backCu = document.getElementById('backCu');
 
 var volUp = document.getElementById('volUp');
+
 var volDown = document.getElementById('volDown');
 
+
+
+// Grab songs
+
+
+
+
+var RobbyEast = document.getElementById('Robby-East');
+var trapN = document.getElementById('trapN');
+var RetroV = document.getElementById('RetroV-Puzz');
+
+
+
+
+RobbyEast.onclick = function(){
+  playerIco.src = "images/artists/OnTheHouse.png";
+  audio.src = "audio/Robby East - Leap of Faith (Original Mix).mp3";
+  audio.play();
+}
+
+trapN.onclick = function(){
+  playerIco.src = "images/artists/trapN.png";
+  audio.src = "audio/music2.mp4";
+  audio.play();
+}
+
+RetroV.onclick = function(){
+  playerIco.src = "images/artists/OnTheHouse.png";
+  audio.src = "audio/RetroVision-Puzzle.mp3";
+  audio.play();
+}
+
+
+// Grab Icon and progress bar
 
 var playerIco = document.getElementById('mIcon');
 
 var progressBar = document.getElementById('progressBar');
 
+var progress_value = 0;
+
+
+var curr_tim = document.getElementById('curr_tim');
+var dur_tim = document.getElementById('dur_tim');
+
+//Define audio
+
+
+
 var isPlaying = false;
-var audio = new Audio("audio/Robby East - Leap of Faith (Original Mix).mp3");
+var audio = new Audio("audio/RetroVision-Puzzle.mp3");
+
+
+
+// Check if buttns are pressed
+
+
+var volUPress = false;
+var volDPress = false;
+
+
+
+
+
+
+
+//Init when time updates
+
+
 audio.ontimeupdate = function(){
   init();
 }
 
 
 
+function init(){
+    progress_value = parseInt(audio.currentTime) / parseInt(audio.duration);
+    progressBar.value = progress_value;
+}
 
-var shit = 0;
+
+
+
+
+// button functions
+
 
 playB.onclick = function(){
   if(isPlaying === false){
@@ -39,31 +118,77 @@ playB.onclick = function(){
 
 
 skipCu.onclick = function(){
-  audio.currentTime += 5;
+  audio.currentTime += 3;
 }
 
 backCu.onclick = function(){
-  audio.currentTime -= 5;
+  audio.currentTime -= 3;
 }
 
 
-volUp.onclick = function(){
-  audio.volume += 0.1;
+volUp.onmousedown = function(){
+  volUPress = true;
 }
 
-volDown.onclick = function(){
-  audio.volume -= 0.1;
+volUp.onmouseup = function(){
+  volUPress = false;
+}
+
+volDown.onmousedown = function(){
+  volDPress = true;
+}
+
+volDown.onmouseup = function(){
+  volDPress = false;
 }
 
 
 
 
+// The update function gets called 4 times a second
 
 
+function update(){
 
 
+  var curr_minutes = Math.floor(audio.currentTime / 60);
 
-function init(){
-    shit = parseInt(audio.currentTime) / parseInt(audio.duration);
-    progressBar.value = shit;
+  var curr_seconds = Math.floor(audio.currentTime) - curr_minutes * 60;
+
+  if(curr_seconds < 10){
+    curr_seconds = "0" + curr_seconds;
+  }
+
+  if(curr_minutes < 10){
+    curr_minutes = "0" + curr_minutes;
+  }
+
+  var dur_minutes = Math.floor(audio.duration / 60);
+
+  var dur_seconds = Math.floor(audio.duration) - dur_minutes * 60;
+
+  if(dur_seconds < 10){
+    dur_seconds = "0" + dur_seconds;
+  }
+
+  if(dur_minutes < 10){
+    dur_minutes = "0" + dur_minutes;
+  }
+
+
+  dur_tim.innerHTML = dur_minutes + ':' + dur_seconds;
+
+  curr_tim.innerHTML = curr_minutes + ':' + curr_seconds;
+  if(volUPress){
+    if(audio.volume < 1)
+      audio.volume += 0.1;
+  }
+
+  if(volDPress){
+    if(audio.volume > 0.1)
+    audio.volume -= 0.1;
+  }
+
+  console.clear();
 }
+setInterval(update, 1000/4);
